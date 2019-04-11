50f47b3219a8cfcfc71fdbdbc8cfc4111e9463e6
Punith M <punithmjpg@gmail.com>
2019-04-11 09:41:07 +0530

    start stop terminate vm

--- 4.sh
+++ 4.sh
@@ -1,4 +1,4 @@
-aws ec2 run-instances --image-ami-0a574895390037a62 --count 1 --instance-type t2.micro --key-name Bit --security-group-ids sg-012040563a45cc550 --subnet-id subnet-6af0ba02
+aws ec2 run-instances --image- ami-0a574895390037a62 --count 1 --instance-type t2.micro --key-name Bit --security-group-ids sg-012040563a45cc550 --subnet-id subnet-6af0ba02
 
 aws ec2 stop-instances --instance-ids i-0e0f87abf0e866c0a
 
